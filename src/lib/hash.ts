cb3c60b0cf1d34a62618f8393ecf37b65ff14ce5
Antima <antima142005@gmail.com>
2025-08-06 02:59:23 +0000

    .tsimport bcrypt from 'bcryptjs';
    export async function hashPassword(pa

--- src/lib/hash.ts
+++ src/lib/hash.ts
@@ -2,8 +2,7 @@ import bcrypt from 'bcryptjs';
 
 export async function hashPassword(password: string) {
     const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-    return hashedPassword;
+    return bcrypt.hash(password, salt);
 }
 
 export async function comparePassword(password: string, hash: string) {
